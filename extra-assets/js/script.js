418a3ad9313a24a5e43d05749cb430b6f065b18d
3dmotion <93018458+xchrismmgx@users.noreply.github.com>
2026-01-28 18:38:56 -0600

    Update script.js

--- extra-assets/js/script.js
+++ extra-assets/js/script.js
@@ -219,6 +219,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const span = document.createElement("div");
         span.className = "view-label";
         span.innerText = label;
+        span.dataset.viewIndex = index; // Para CSS específico
         // Calcular posición: 0% abajo, 100% arriba
         const pct = (index / (zone.viewLabels.length - 1)) * 100;
         span.style.bottom = `${pct}%`;
@@ -318,5 +319,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  init();
-});
+  i
